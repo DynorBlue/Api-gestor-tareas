07a3553d1f05505037762ff2b0f9300a86509514
DynorBlue <dynorblue@gmail.com>
2025-09-15 07:30:33 -0600

    creacion proyecto

---
+++ GestorTareas/src/test/java/com/DynorBlue/ApiGestorTareas/GestorTareas/GestorTareasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.DynorBlue.ApiGestorTareas.GestorTareas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestorTareasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
